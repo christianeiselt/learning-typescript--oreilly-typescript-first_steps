b494f147e7164f10040ebe51b68d0c7d19abb844
Christian Eiselt <dev@eiselt.ch>
2021-04-02 16:08:24 +0200

    Add class exercise

--- person.ts
+++ person.ts
@@ -8,6 +8,31 @@ interface Employee extends Person {
   doWork(): void;
 }
 
+class Manager implements Employee {
+  title?: string | undefined;
+  doWork(): void {
+    console.log("Manager at work.");
+  }
+  name: string;
+  age: number;
+
+  constructor(public department: string, emp_name: string, emp_age: number) {
+    this.name = emp_name;
+    this.age = emp_age;
+  }
+}
+
+let mgr: Manager = new Manager("WebDev", "Michelle", 30);
+mgr.doWork();
+console.log(mgr);
+
+mgr = {
+  department: "Web Dev",
+  name: "Daniel",
+  age: 48,
+  doWork: () => console.log("Busy working..."),
+};
+
 let engineer: Employee = {
   name: "Daniel",
   age: 48,
